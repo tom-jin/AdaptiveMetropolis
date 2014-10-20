73afa582eee5332d255723b23e581aee941259fa
Tom Jin <sjin@stats.ox.ac.uk>
2014-10-20 11:19:14 +0100

    Stub unit tests. Checking each algorithm can fit a normal.

---
+++ tests/testthat/test.RAM.R
@@ -0,0 +1,6 @@
+test_that("RAM can find the parameters of a normal distribution", {
+  data <- RAM(function(x) {dnorm(x, 100,10)}, 1100, 0, 1)
+  
+  expect_equal(object = mean(data[101:1100]), expected = 100, tolerance = 0.5)
+  expect_equal(object = sd(data[101:1100]), expected = 10, tolerance = 0.5)
+})
