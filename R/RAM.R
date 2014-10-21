673918b31fd38abb0abe5e9c2c6894b51c83d01c
Tom Jin <sjin@stats.ox.ac.uk>
2014-10-21 16:37:25 +0100

    Using = as an assignment operator is discouraged.

--- R/RAM.R
+++ R/RAM.R
@@ -10,24 +10,27 @@
 #' @export
 RAM <- function(target, n, init.state, init.cov)  {
   d <- length(init.state)
-  res=matrix(,n,d)
-  res[1,] = rep(0,d)
-  t0 = n/2
-  alpha_star = 0.234
-  S=diag(d)
+  res <- matrix(,n,d)
+  res[1,] <- rep(0,d)
+  t0 <- n/2
+  alpha_star <- 0.234
+  S <- diag(d)
   
   for (t in 2:n) {
-    mean = res[t-1,]  
-    U = rnorm(d)
-    eta = min(1,d*t^(-2/3))
-    x = mean + S %*% U
-    alpha = min(1,target(x)/target(res[t-1,]))
-    u = runif(1)
-    if (u<alpha) {res[t,] = x}
-    else {res[t,] = res[t-1,]}
+    mean <- res[t-1,]  
+    U <- rnorm(d)
+    eta <- min(1,d*t^(-2/3))
+    x <- mean + S %*% U
+    alpha <- min(1,target(x)/target(res[t-1,]))
+    u <- runif(1)
     
-    sigma = S %*% (diag(d) + eta*(alpha-alpha_star)* U %*% t(U)/(sum(U^2))) %*% t(S)  
-    S = t(chol(sigma))  
+    if (u<alpha) 
+      res[t,] <- x
+    else 
+      res[t,] <- res[t-1,]
+    
+    sigma <- S %*% (diag(d) + eta*(alpha-alpha_star)* U %*% t(U)/(sum(U^2))) %*% t(S)  
+    S <- t(chol(sigma))  
   }    
   return(res)
 }  
