b2806dfc9f22ee794560470612b29ecce5da7ad6
Tom Jin <sjin@stats.ox.ac.uk>
2014-10-22 09:25:23 +0000

    Comment and tidy AM and ASWAM

--- R/ASWAM.R
+++ R/ASWAM.R
@@ -12,33 +12,32 @@ ASWAM <- function(target, n, init.state, init.cov) {
   d <- length(init.state)
   X <- matrix(NA, nrow = n + 1, ncol = d)
   X[1, ] <- init.state
-
   lambda <- 1
   sigma <- init.cov
   
   for(i in seq(1, n)) {
+    # Propose a move
     Y <- as.vector(rmvnorm(n = 1, mean = X[i, ], sigma = as.matrix(lambda * sigma)))
+    
+    # Calculate acceptance ratio
     a <- min(1, target(Y)/target(X[i, ])) 
     
     if(runif(1) < a) 
-      X[i+1, ] <- Y
+      X[i+1, ] <- Y # Accept
     else
-      X[i+1, ] <- X[i, ]
+      X[i+1, ] <- X[i, ] # Reject
     
-    if(d == 1) {
-      Xbarold <- mean(X[1:i, ])
-      Xbarnew <- mean(X[seq(1, i+1), ])
-    } else {
-      Xbarold <- .colMeans(X[1:i, ], i, d)
-      Xbarnew <- .colMeans(X[seq(1, i+1), ], i+1, d)
-    }
+    Xbarold <- .colMeans(X[1:i, ], i, d)
+    Xbarnew <- .colMeans(X[seq(1, i+1), ], i+1, d)
     
+    # Update scaling ratio
     lambda <- exp(log(lambda) + a - 0.234)
+    
+    # Update covariance
     sigma <- ((i-1)/i) * sigma + (2.4^2)/(d*i) * ((i * Xbarold %*% t(Xbarold))
                                           - ((i + 1 ) * Xbarnew %*% t(Xbarnew)) 
                                           + (X[i+1, ] %*% t(X[i+1, ])) 
                                           + .Machine$double.eps*diag(d))
   }
-  
   return(X[-1, ])
 }
