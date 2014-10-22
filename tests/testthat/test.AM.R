81782b39cdfb390ff8cc98f2d7be5e70819e2775
Tom Jin <sjin@stats.ox.ac.uk>
2014-10-22 09:11:27 +0000

    Test the 2D case

--- tests/testthat/test.AM.R
+++ tests/testthat/test.AM.R
@@ -1,6 +1,15 @@
-test_that("AM can find the parameters of a normal distribution", {
-  data <- AM(function(x) {dnorm(x, 100,10)}, 1100, 0, 1, 100)
+test_that("AM passes 1D sanity test", {
+  set.seed(0)
+  data <- AM(function(x) {dnorm(x, 100,10)}, 5000, 0, 1)
 
-  expect_equal(object = mean(data[101:1100]), expected = 100, tolerance = 0.5)
-  expect_equal(object = sd(data[101:1100]), expected = 10, tolerance = 0.5)
+  expect_equal(object = mean(data), expected = 100, tolerance = 0.5)
+  expect_equal(object = sd(data), expected = 10, tolerance = 0.5)
+})
+
+test_that("AM passes 2D sanity test", {
+  set.seed(0)
+  data <- AM(function(x){dmvnorm(x, c(10, 5), matrix(c(10, 2, 2, 10), 2, 2))}, 5000, rep(0, 2), diag(2))
+  
+  expect_equal(object = colMeans(data), expected = c(10, 5), tolerance = 0.5)
+  expect_equal(object = cov(data), expected = matrix(c(10, 2, 2, 10), 2, 2), tolerance = 0.5)
 })
