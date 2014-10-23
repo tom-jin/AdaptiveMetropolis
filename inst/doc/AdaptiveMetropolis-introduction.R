0114ffc612158fa00217f10dffa9379daa36df8c
Xiaoyu Lu <xlu@stats.ox.ac.uk>
2014-10-23 01:51:13 +0000

    RAM and AMWG report

--- inst/doc/AdaptiveMetropolis-introduction.R
+++ inst/doc/AdaptiveMetropolis-introduction.R
@@ -78,3 +78,60 @@ data <- ASWAM(function(x) {dmvnorm(x, rep(10, 8), 10*diag(8))},
               1000, rep(0, 8), diag(8))
 pairs(data)
 
+## ----chunk1--------------------------------------------------------------
+library(mvtnorm)  
+init.state = c(0,0,0)  
+d = length(init.state) 
+init.cov = diag(d)      
+target = function(x) {  
+   return(dmvnorm(t(x),rep(0,d),diag(d)))  
+}
+
+res <- RAM(target,10000,init.state,init.cov)
+
+## ----chunk2--------------------------------------------------------------
+par(mfrow=c(2,1))
+plot(res[,1],type = "l",xlab="iteration",main="traceplot ")
+hist(res[,1],main="histogram")
+
+## ----chunk3--------------------------------------------------------------
+out_unadapt <- AMWG(adapt = 0, 1000, 500)
+theta_unadapt <- out_unadapt[[1]]
+acceptance_unadapt <- out_unadapt[[2]]
+out_adapt <- AMWG(adapt = 1, 1000, 500)
+theta_adapt <- out_adapt[[1]]
+acceptance_adapt <- out_adapt[[2]]
+
+plot(theta_unadapt[,1],type="l",xlab="number of batch",ylab=expression(theta[1]),main=expression('traceplots of '*theta[1]*' using Metropolis within Gibbs'))
+lines(theta_adapt[,1],col=2)
+legend("topright",c("unadapted","adapted"),col=c(1,2),lwd=c(2,2))
+
+## ----chunk4--------------------------------------------------------------
+par(mfrow=c(2,1))
+hist(colMeans(acceptance_unadapt),main="histogram of acceptance ratio for K+3 parameters (unadapted)",xlab="",breaks=50)
+hist(colMeans(acceptance_adapt),main="histogram of acceptance ratio for K+3 parameters (adapted)",xlab="",breaks=50)
+
+## ----result = F----------------------------------------------------------
+library(mnormt)
+library(lattice)
+library(coda)
+
+## ------------------------------------------------------------------------
+N=10000
+init.state = rnorm(2)    #random initialization
+d = length(init.state)
+init.cov = diag(d)
+mean = c(1,2)
+sigma = matrix(c(0.2,0.1,0.1,0.8),2,2)
+target = function(x) {
+  return(dmt(t(x),mean,sigma,1))
+}
+
+resAM <- AM(target, N, init.state, init.cov)
+resASWAM <- ASWAM(target, N, init.state, init.cov)
+resRAM <- RAM(target, N, init.state, init.cov)
+
+plot(resAM[,1],resAM[,2])
+plot(resASWAM[,1],resASWAM[,2])
+plot(resRAM[,1],resRAM[,2])
+
