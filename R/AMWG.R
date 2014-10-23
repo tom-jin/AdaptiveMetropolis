0a5355d4ae5d12929f6f77c29b9bc0e1c591b6c6
Xiaoyu Lu <xlu@stats.ox.ac.uk>
2014-10-23 00:51:21 +0000

    Edit RAM and AMWG

--- R/AMWG.R
+++ R/AMWG.R
@@ -7,7 +7,7 @@
 #' @param adapt whether to use adapted MH or normal MH algorithm
 #' @param N number of iterations
 #' @param K number of latent variables
-#' @return A list containing simulated parameters paths (theta) and acceptance ratio paths (acceptance).
+#' @return A list containing simulated parameters paths (theta) and acceptance/rejection paths (acceptance).
 #' @references Roberts, G. O., Rosenthal, J. S. (2006), \emph{Examples of Adaptive MCMC}
 #' @author Xiaoyu Lu
 #' @export
@@ -20,56 +20,60 @@
 #' points(theta_adapt[,1],col=2)
 
 AMWG <- function(adapt, N, K) {
-    acceptance <- matrix(0, N, K+3)
-    
-    # Initial condition
-    theta <- matrix(, N, K+3);theta[1, ] <- rep(100, K+3)
-    alpha <- c()
-    ls <- rep(0, K+3)
-    
-    #Simulated data
-    r <- sample(5:500, size = K, replace = TRUE)
-    Y <- list()
-    for (i in 1:K) {
-      Y[[i]] <- rnorm(r[i]) * 10 + (i-1) 
-    }
-    
-    
-    for(n in 1:(N-1))  {
-      delta <- min(0.005, n^(-1/2))
-      
-      if(n > 50) {
-        index <- which(colSums(acceptance)/n > 0.44)
-        ls[index] <- ls[index]+delta*adapt
-        ls[setdiff(1:(K+3), index)] <- ls[setdiff(1:(K+3), index)]-delta*adapt
-      }
-      
-      for(k in 1:(K+3)) {
-        ls[k] <- max(min(ls[k], 3), -3)
-      }
-      
-      theta_new <- rnorm(K+3) * exp(ls) + theta[n, ]
-      u <- runif(K+3)
-      for (i in 1:K) {    
-        alpha[i] <- min(1, (1+((theta[n, i]-theta[n, K+3])/theta[n, K+1])^2)/(1+((theta_new[i]-theta[n, K+3])/theta[n, K+1])^2) *
-                          exp(1/(2*theta[n, K+2])*sum((Y[[i]]-theta[n, i])^2-(Y[[i]]-theta_new[i])^2)))       
-      }
-      
-      alpha[K+1] <- min(1, exp(-1/theta_new[K+1]+1/theta[n, K+1])*(theta[n, K+1]/theta_new[K+1])^2 * ifelse(theta_new[K+1] >= 0, 1, 0)
-                        * prod(1+((theta[n, 1:K]-theta[n, K+3])/theta[n, K+1])^2)/prod(1+((theta_new[1:K]-theta[n, K+3])/theta[n, K+1])^2), na.rm = T)
-      
-      alpha[K+2] <- min(1, (theta[n, K+2]/theta_new[K+2])^(sum(r)/2+2) * ifelse(theta_new[K+2]>=0, 1, 0) *
-                          exp((1/theta[n, K+2]-1/theta_new[K+2])*(1+sum((unlist(Y)-rep(theta[n, 1:K], r))^2)/2)), na.rm=T)
-      
-      alpha[K+3] <- min(1, exp(-theta_new[K+3]^2/2+theta[n, K+3]^2/2) 
-                        * prod(1+((theta[n, 1:K]-theta[n, K+3])/theta[n, K+1])^2)/prod(1+((theta_new[1:K]-theta[n, K+3])/theta[n, K+1])^2), na.rm = T)
-      
-      index1 <- which(u<alpha)
-      index2 <- setdiff(1:(K+3), index1)
-      theta[n+1, index1] <- theta_new[index1]
-      theta[n+1, index2] <- theta[n, index2]
-      acceptance[n, which(u<alpha)] <- 1
-      
-    }
-    return(list(theta, acceptance))
+  acceptance <- matrix(0, N, K+3)
+  
+  # Initial condition
+  theta <- matrix(, N, K+3);theta[1, ] <- rep(100, K+3)
+  alpha <- c()
+  ls <- rep(0, K+3)
+  
+  #Simulated data
+  r <- sample(5:500, size = K, replace = TRUE)
+  Y <- list()
+  for (i in 1:K) {
+    Y[[i]] <- rnorm(r[i]) * 10 + (i-1) 
   }
+  
+  #Gibbs sampler
+  for(n in 1:(N-1))  {
+    delta <- min(0.001, n^(-1/2))
+    
+    if(n > 50) {
+      # adjust the proposal deviation according to the proportion of accepted proposals up to time n
+      index <- which(colSums(acceptance)/n > 0.44)
+      ls[index] <- ls[index]+delta*adapt
+      ls[setdiff(1:(K+3), index)] <- ls[setdiff(1:(K+3), index)]-delta*adapt
+    }
+    
+    #bound the standard deivation
+    for(k in 1:(K+3)) {
+      ls[k] <- max(min(ls[k], 3), -3)
+    }
+    
+    #propose new moves
+    theta_new <- rnorm(K+3) * exp(ls) + theta[n, ]
+    u <- runif(K+3)
+    # append A, V, mu to theta and calculate acceptance probabilities
+    for (i in 1:K) {    
+      alpha[i] <- min(1, (1+((theta[n, i]-theta[n, K+3])/theta[n, K+1])^2)/(1+((theta_new[i]-theta[n, K+3])/theta[n, K+1])^2) *
+                        exp(1/(2*theta[n, K+2])*sum((Y[[i]]-theta[n, i])^2-(Y[[i]]-theta_new[i])^2)))       
+    }
+    
+    alpha[K+1] <- min(1, exp(-1/theta_new[K+1]+1/theta[n, K+1])*(theta[n, K+1]/theta_new[K+1])^2 * ifelse(theta_new[K+1] >= 0, 1, 0)
+                      * prod(1+((theta[n, 1:K]-theta[n, K+3])/theta[n, K+1])^2)/prod(1+((theta_new[1:K]-theta[n, K+3])/theta[n, K+1])^2), na.rm = T)
+    
+    alpha[K+2] <- min(1, (theta[n, K+2]/theta_new[K+2])^(sum(r)/2+2) * ifelse(theta_new[K+2]>=0, 1, 0) *
+                        exp((1/theta[n, K+2]-1/theta_new[K+2])*(1+sum((unlist(Y)-rep(theta[n, 1:K], r))^2)/2)), na.rm=T)
+    
+    alpha[K+3] <- min(1, exp(-theta_new[K+3]^2/2+theta[n, K+3]^2/2) 
+                      * prod(1+((theta[n, 1:K]-theta[n, K+3])/theta[n, K+1])^2)/prod(1+((theta_new[1:K]-theta[n, K+3])/theta[n, K+1])^2), na.rm = T)
+    
+    index1 <- which(u<alpha)
+    index2 <- setdiff(1:(K+3), index1)
+    theta[n+1, index1] <- theta_new[index1]
+    theta[n+1, index2] <- theta[n, index2]
+    acceptance[n, which(u<alpha)] <- 1
+    
+  }
+  return(list(theta, acceptance))
+}
--- R/RAM.R
+++ R/RAM.R
@@ -7,33 +7,33 @@
 #' @param init.cov The initial covariance of the sampler.
 #' @return n samples from the target distribution.
 #' @export
-RAM <- function(target, n, init.state, init.cov)  {
-  # Input validation
-  validateInput(target, n, init.state, init.cov)
+
+#RAN function takes inputs including target distribution, number of iterations, initial state and initial proposal covariance matrix
+RAM <- function(target, N, init.state, init.cov)  {
   
-  # Init variables
-  d <- length(init.state)
-  res <- matrix(,n,d)
-  res[1,] <- rep(0,d)
-  t0 <- n/2
-  alpha_star <- 0.234
-  S <- diag(d)
+  d <- length(init.state)  # dimension of the space of interest
+  res=matrix(,N,d)         # set up the result matrix, where each row n represents the parameter of length d at time n 
+  res[1,] = init.state     # store the initial state as the first result
+  t0 = N/2                 # burn-in period is set to be N/2, where N is the number of iterations
+  alpha_star = 0.234       # target mean acceptance rate is set to be 0.234 according to some popular theoretical result
+  S=diag(d)                # initial covariance is set to be the identity matrix, i.e. components are independent
   
-  for (t in 2:n) {
-    mean <- res[t-1,]  
-    U <- rnorm(d)
-    eta <- min(1,d*t^(-2/3))
-    x <- as.vector(mean + S %*% U)
-    alpha <- min(1,target(x)/target(res[t-1,]))
-    u <- runif(1)
+  for (t in 2:N) {
+    eta = min(1,d*t^(-2/3))     # set eta
     
-    if (u<alpha) 
-      res[t,] <- x
-    else 
-      res[t,] <- res[t-1,]
+    mean = res[t-1,]       # set the mean for the proposal to be the current state
+    U = rnorm(d)           # simulate a d-dimensional MVN variable
+    x = mean + S %*% U          # calculate the proposed point
+    alpha = min(1,target(x)/target(res[t-1,]))     #calculate acceptance probability
+    u = runif(1)                # simulate a uniform random variable and accept/reject the proposal accordingly.
+    if (u<alpha) {res[t,] = x}
+    else {res[t,] = res[t-1,]}
     
-    sigma <- S %*% (diag(d) + eta*(alpha-alpha_star)* U %*% t(U)/(sum(U^2))) %*% t(S)  
-    S <- t(chol(sigma))  
+    sigma = S %*% (diag(d) + eta*(alpha-alpha_star)* U %*% t(U)/(sum(U^2))) %*% t(S)  #update the covariance matrix
+    S = t(chol(sigma))  
   }    
-  return(res)
+  
+  return(res)         # output the simulated path
 }  
+
+
